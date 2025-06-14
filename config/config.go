66d8795dbaf787ad8e4ae7ef6dfe51e5c44cdec9
Phool Prakash <91048057+Phool-Prakash@users.noreply.github.com>
2025-06-14 15:16:57 +0530

    dbImprovement

--- config/config.go
+++ config/config.go
@@ -13,15 +13,15 @@ import (
 
 //this is for batter logging
 var once sync.Once
-var cachedConfig *Config
+var cachedCfg *Config
 
 //Config Structure (this is encapsulate the important settings)
 type Config struct {
 	Port                   string
 	MongoDBURI             string
-	JWTSecret              string // For Access Token
-	RefreshTokenSecret     string // NEW: For Refresh Token
-	RefreshTokenLifetimeHours int // NEW: Refresh Token lifetime
+	JWTSecret              string 
+	RefreshTokenSecret     string 
+	RefreshTokenLifetimeHours int 
 	TwilioAccountSID       string
 	TwilioAuthToken        string
 	TwilioPhoneNumber      string
@@ -37,7 +37,7 @@ func LoadConfig() *Config {
 		log.Println("Error loading .env file, assuming environment variables are set")
 	}
 
-	cachedConfig =  &Config{
+	cachedCfg =  &Config{
 		Port:                   getEnv("PORT", ":8080"),
 		MongoDBURI:             getEnv("MONGODB_URI", "mongodb://localhost:27017/propertyAppDatabase"),
 		JWTSecret:              getSecureEnv("JWT_SECRET"),
@@ -49,7 +49,7 @@ func LoadConfig() *Config {
 		OTPLifetimeMinutes:     parseIntEnv("OTP_LIFETIME_MINUTES",2),
 	}
 	})
-	return cachedConfig
+	return cachedCfg
 }
 
 //Secure environment variable retrieval
--- db/db.go
+++ db/db.go
@@ -1,41 +1,100 @@
+// This file handle MongoDB connection and Database Collections
+// To estiblish the database connection and return the specific collections
+// package database
+
+// import (
+// 	"context"
+// 	"log"
+// 	"sync"
+
+// 	"go.mongodb.org/mongo-driver/mongo"
+// 	"go.mongodb.org/mongo-driver/mongo/options"
+// )
+
+// var once sync.Once
+// var cachedClient *mongo.Client
+// var Ctx = context.Background()
+
+// func ConnectDB(uri string) (*mongo.Client, error) {
+// 	var err error
+// 	once.Do(func ()  {
+// 		clientOptions := options.Client().ApplyURI(uri).SetMaxPoolSize(50)
+// 		_, err := mongo.Connect(Ctx,clientOptions)
+// 	if err != nil {
+// 		log.Fatal("Database connection failed:",err)
+// 	} 
+// 	})
+// 	return cachedClient, err
+// }
+
+// // GetUserCollection returns the users collection
+// func GetUserCollection(client *mongo.Client) *mongo.Collection {
+// 	return client.Database("propertyAppDatabase").Collection("users")
+// }
+
+// // GetOTPCollection returns the otps collection
+// func GetOTPCollection(client *mongo.Client) *mongo.Collection {
+// 	return client.Database("propertyAppDatabase").Collection("otps")
+// }
+
+// func GetRefreshTokenCollection(client *mongo.Client) *mongo.Collection { // <-- NEW
+// 	return client.Database("propertyAppDatabase").Collection("refresh_tokens")
+// }
+
+
+
 package database
 
 import (
-	"context"
-	"time"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+    "context"
+    "log"
+    "sync"
+
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var once sync.Once
+var cachedClient *mongo.Client
 var Ctx = context.Background()
 
+// ConnectDB with caching and retry mechanism
 func ConnectDB(uri string) (*mongo.Client, error) {
-	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
-	if err != nil {
-		return nil, err
-	}
+    var err error
+    once.Do(func() {
+        clientOptions := options.Client().ApplyURI(uri).SetMaxPoolSize(50)
 
-	ctx, cancel := context.WithTimeout(Ctx, 10*time.Second)
-	defer cancel()
+        cachedClient, err = mongo.Connect(Ctx, clientOptions)
+        if err != nil {
+            log.Fatal("Database connection failed:", err)
+        }
+    })
 
-	err = client.Connect(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	return client, nil
+    return cachedClient, err
 }
 
-// GetUserCollection returns the users collection
+// Get specific collections
+
+//GetUserCollection returns the users collection
 func GetUserCollection(client *mongo.Client) *mongo.Collection {
-	return client.Database("propertyAppDatabase").Collection("users")
+	if cachedClient == nil {
+		log.Println("Database client not initialized!")
+	}
+    return client.Database("propertyAppDatabase").Collection("users")
 }
 
-// GetOTPCollection returns the otps collection
 func GetOTPCollection(client *mongo.Client) *mongo.Collection {
-	return client.Database("propertyAppDatabase").Collection("otps")
+	if cachedClient == nil {
+		log.Println("Database client not initialized!")
+
+	}
+    return client.Database("propertyAppDatabase").Collection("otps")
 }
 
-func GetRefreshTokenCollection(client *mongo.Client) *mongo.Collection { // <-- NEW
-	return client.Database("propertyAppDatabase").Collection("refresh_tokens")
+func GetRefreshTokenCollection(client *mongo.Client) *mongo.Collection {
+	if cachedClient == nil {
+		log.Println("Database client not initialized!")
+
+	}
+    return client.Database("propertyAppDatabase").Collection("refresh_tokens")
 }
